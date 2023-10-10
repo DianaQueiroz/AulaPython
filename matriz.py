7aa795d4eb65f610ebfeb3adaa7e27cccd7ca1f7
Diana Queiroz <136858223+DianaQueiroz@users.noreply.github.com>
2023-10-09 21:16:56 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,13 @@
+matriz=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+# elemento=matriz[2][1]
+# print(elemento)
+
+num_linhas= len(matriz)
+num_colunas= len(matriz[0])
+print(num_linhas)
+print(num_colunas)
